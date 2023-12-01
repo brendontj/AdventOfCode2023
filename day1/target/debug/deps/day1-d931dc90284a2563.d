c875d5e3075800cdda56179468d7635457b086fc
Brendon Henrique <brendonhps@gmail.com>
2023-12-01 20:45:36 -0300

    day 1 done

--- day1/target/debug/deps/day1-d931dc90284a2563.d
+++
@@ -1,5 +0,0 @@
-/home/brendontj/projects/aoc2023/day1/target/debug/deps/day1-d931dc90284a2563.rmeta: src/main.rs
-
-/home/brendontj/projects/aoc2023/day1/target/debug/deps/day1-d931dc90284a2563.d: src/main.rs
-
-src/main.rs:
